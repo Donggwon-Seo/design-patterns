f69c90c933348624aa31b664fa598f0fa1732c25
SeoDonggwon <ehdrnjsdl920@naver.com>
2022-11-01 20:31:16 +0900

    Order Process Switch-expression 적용

--- src/clothesShopToyProject/process/Order.java
+++ src/clothesShopToyProject/process/Order.java
@@ -3,45 +3,60 @@ package clothesShopToyProject.process;
 import clothesShopToyProject.product.Product;
 import clothesShopToyProject.productfactory.*;
 import clothesShopToyProject.signup.*;
+import clothesShopToyProject.transform.TransformEnumOptions;
+import clothesShopToyProject.transform.TransformEnumOrder;
+import clothesShopToyProject.transform.TransformEnumProducts;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Scanner;
 
 public class Order {
     Product product;
-    loginAuthentication login = new loginAuthentication();
+    LoginAuthentication login = new LoginAuthentication();
     ClothesFactory clothesFactory = new ClothesFactory();
     EtcFactory etcFactory = new EtcFactory();
     OptionFactory optionFactory = new OptionFactory();
     PantsFactory pantsFactory = new PantsFactory();
+    //    TransformEnumProducts tfProduct;
+//    TransformEnumOptions tfOption;
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     private static boolean exit = true;
     private static boolean etcCheck = false;
+    private static boolean selectOptionCheck = true;
     int colorCheck = 1;
 
     public Order() throws ConditionInputException, IOException {
         do {
+            // console에 보여주는 클래스들을 별도로 구현
+            // colsole에 보여주는 방법을 구현하는 비즈니스 로직들을 따로 구현
             System.out.println("--- 1. 회원가입 및 로그인 | 2. 상품 주문 | 3. 종료 ---");
-            int selectOrder = Integer.parseInt(br.readLine());
+            String selectOrder = br.readLine();
 
-            if (selectOrder == 1) {
-                login.process();
-            } else if (selectOrder == 2 && login.getLoginStatus()) {
-                selectProduct();
-                if (etcCheck) {
-                    System.out.println(product.getBrand() +" "+ product.getDescription()+"의 주문이 완료되었습니다.");
-                    break;
+            switch (TransformEnumOrder.transform(selectOrder)) {
+                case LOGIN_AND_SIGNUP -> {
+                    login.process();
                 }
-                selectOptions();
-            } else if (selectOrder == 2 && login.getLoginStatus() == false) {
-                System.out.println("로그인을 진행하여 주세요.");
-                login.process();
-            } else if (selectOrder == 3) {
-                exit = false;
-            } else
-                throw new ConditionInputException("1과 2의 숫자만 입력하여 주세요");
+                case ORDER -> {
+                    if (login.getLoginStatus()) { // STATE PATTERN 사용해보기 -> 공부하기
+                        selectProduct();
+                        if (etcCheck) {
+                            System.out.println(product.getBrand() + " " + product.getDescription() + "의 주문이 완료되었습니다.");
+                            break;
+                        }
+                        selectOptions();
+                    } else {
+                        System.out.println("로그인을 진행하여 주세요.");
+                        login.process();
+                    }
+                }
+                case EXIT -> {
+                    exit = false;
+                }
+                default -> {
+                    throw new ConditionInputException("");
+                }
+            }
         } while (exit);
     }
 
@@ -49,27 +64,33 @@ public class Order {
         System.out.println("동권 멀티숍에 오신 걸 환영합니다.");
         System.out.println("---- 1. 상의 | 2. 하의 | 3. 악세서리 ----");
 
-        int selectProductNum = Integer.parseInt(br.readLine());
+        String selectProduct = br.readLine();
+        switch (TransformEnumProducts.transform(selectProduct)) {
+            case CLOTHES -> {
+                System.out.println("-------- 원하시는 상의을 선택하여 주세요 --------");
+                System.out.println("---- 1. 스투시 후드티 | 2. 로맨틱크라운 맨투맨 | 3. 8디비전 니트 ----");
 
-        if (selectProductNum == 1) {
-            System.out.println("-------- 원하시는 상의을 선택하여 주세요 --------");
-            System.out.println("---- 1. 스투시 후드티 | 2. 로맨틱크라운 맨투맨 | 3. 8디비전 니트 ----");
+                String selectClothes = br.readLine();
+                product = clothesFactory.createClothes(selectClothes);
+            }
+            case PANTS -> {
+                System.out.println("-------- 원하시는 하의을 선택하여 주세요 --------");
+                System.out.println("---- 1. 청바지 | 2. 슬랙스 | 3. 반바지 ----");
 
-            int selectClothesNum = Integer.parseInt(br.readLine());
-            product = clothesFactory.createClothes(selectClothesNum);
-        } else if (selectProductNum == 2) {
-            System.out.println("-------- 원하시는 하의을 선택하여 주세요 --------");
-            System.out.println("---- 1. 청바지 | 2. 슬랙스 | 3. 반바지 ----");
+                String selectPants = br.readLine();
+                product = PantsFactory.createPants(selectPants);
+            }
+            case ETC -> {
+                System.out.println("-------- 원하시는 악세서리를 선택하여 주세요 --------");
+                System.out.println("---- 1. 시계 | 2. 반지 | 3. 모자 ----");
 
-            int selectPantsNum = Integer.parseInt(br.readLine());
-            product = clothesFactory.createClothes(selectPantsNum);
-        } else if (selectProductNum == 3) {
-            System.out.println("-------- 원하시는 악세서리를 선택하여 주세요 --------");
-            System.out.println("---- 1. 시계 | 2. 반지 | 3. 모자 ----");
-
-            int selectEtcNum = Integer.parseInt(br.readLine());
-            product = etcFactory.createEtc(selectEtcNum);
-            etcCheck = true;
+                String selectEtc = br.readLine();
+                product = etcFactory.createEtc(selectEtc);
+                etcCheck = true;
+            }
+            default -> {
+                throw new ConditionInputException("");
+            }
         }
     }
 
@@ -79,28 +100,41 @@ public class Order {
         do {
             System.out.println("--------------------옵션 선택 창입니다.--------------------");
             System.out.println("----1. 기장 수선 | 2. 색상 변경 : White | 3. 색상 변경 : Red | 4. 옵션을 더 이상 추가하지 않음----");
-            int selectOptionNum = Integer.parseInt(br.readLine());
-            if (selectOptionNum == 1) {
-                product = optionFactory.selectOption(selectOptionNum, product);
-                System.out.println("기장 수선이 추가되었습니다");
-            } else if (selectOptionNum == 2 && colorCheck == 1) {
-                colorCheck--;
-                product = optionFactory.selectOption(selectOptionNum, product);
-                System.out.println("색상을 WHITE로 변경하였습니다.");
-            } else if (selectOptionNum == 3 && colorCheck == 1) {
-                colorCheck--;
-                product = optionFactory.selectOption(selectOptionNum, product);
-                System.out.println("색상을 RED로 변경하였습니다.");
-            } else if (selectOptionNum == 4) {
-                colorCheck--;
-                product = optionFactory.selectOption(selectOptionNum, product);
-                System.out.println("옵션 선택이 종료되었습니다.");
-            } else if (colorCheck == 0) {
-                System.out.println("색상 변경 기회가 없습니다.");
-            } else
-                throw new ConditionInputException("1부터 3사이의 숫자만 입력하세요");
-        } while (colorCheck == 1);
 
+            String selectOption = br.readLine();
+            switch (TransformEnumOptions.transform(selectOption)) {
+                case REFORM -> {
+                    product = optionFactory.selectOption(selectOption, product);
+                    System.out.println("기장 수선이 추가되었습니다");
+                }
+                case COLOR_WHITE -> {
+                    if (colorCheck < 1) {
+                        System.out.println("색상을 변경할 수 없습니다.");
+                        break;
+                    }
+                    colorCheck--;
+                    product = optionFactory.selectOption(selectOption, product);
+                    System.out.println("색상을 WHITE로 변경하였습니다.");
+                }
+                case COLOR_RED -> {
+                    if (colorCheck < 1) {
+                        System.out.println("색상을 변경할 수 없습니다.");
+                        break;
+                    }
+                    colorCheck--;
+                    product = optionFactory.selectOption(selectOption, product);
+                    System.out.println("색상을 RED로 변경하였습니다.");
+                }
+                case EXIT -> {
+                    product = optionFactory.selectOption(selectOption, product);
+                    System.out.println("옵션 선택이 종료되었습니다.");
+                    selectOptionCheck = false;
+                }
+                default -> {
+                }
+            }
+        } while (selectOptionCheck);
+        colorCheck = 1;
         System.out.println(product.getDescription() + ", size :" + product.getSize() + "의 주문이 완료되었습니다.");
     }
 
